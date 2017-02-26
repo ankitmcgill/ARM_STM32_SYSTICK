a1ef24e0cef3d22e2d5cf8cceab338c61a4ee76d
Ankit Bhatnagar <ankit.bhatnagarindia@gmail.com>
2017-02-27 00:45:32 +0530

    final ARM STM32 SYSTICK. tested working. also added git ignore file. Also added function to get stm32 arm 96 bit unique id

--- ARM_STM32_SYSTICK.c
+++ ARM_STM32_SYSTICK.c
@@ -8,3 +8,66 @@
 /////////////////////////////////////////////////////
 
 #include "ARM_STM32_SYSTICK.h"
+
+uint32_t ARM_STM32_SYSTICK_SOURCE;
+
+void ARM_STM32_SYSTICK_Read_Device_Id(uint32_t* ptr)
+{
+	//READ AND RETURN 96 BITS (12 BYTES)
+	//UNIQUE DEVICE ID
+	
+	uint32_t* ptr_val = ((uint32_t*)DEVICE_UNIQUE_ID_ADDRESS);
+	
+	ptr[0] = ptr_val[0];
+	ptr[1] = ptr_val[1];
+	ptr[2] = ptr_val[2];
+}
+
+void ARM_STM32_SYSTICK_Init(uint32_t systick_clock_source)
+{
+	//SET THE SOURCE OF SYSTICK (HCLK OR HCLK/8)
+	SysTick_CLKSourceConfig(systick_clock_source);
+	
+	ARM_STM32_SYSTICK_SOURCE = systick_clock_source;
+}
+
+void ARM_STM32_SYSTICK_Delay_Us(uint32_t val)
+{
+	//BLOCKING DELAY OF SPECIFIED MICRO-SECONDS
+	
+	RCC_ClocksTypeDef clock;
+	RCC_GetClocksFreq(&clock);
+	
+	//FIND THE SYSTICK COUNT REQUIRED FOR 1US PERIOD
+	uint32_t one_us_count;
+	if(ARM_STM32_SYSTICK_SOURCE == SysTick_CLKSource_HCLK)
+	{
+		one_us_count = (clock.HCLK_Frequency/1000000);
+	}
+	else
+	{
+		one_us_count = (clock.HCLK_Frequency/8000000);
+	}
+	
+	SysTick->VAL=0;
+	SysTick->LOAD = one_us_count;
+	SysTick->CTRL &= ~(SysTick_CTRL_TICKINT_Msk);
+	SysTick->CTRL |= SysTick_CTRL_ENABLE_Msk;
+	
+	while(val != 0)
+	{
+		while((SysTick->CTRL & SysTick_CTRL_COUNTFLAG_Msk) == 0){};
+		val--;
+	}
+	
+	//SYSTICK TIME DONE. DISABLE IT
+	SysTick->CTRL &= ~(SysTick_CTRL_ENABLE_Msk);
+}
+
+void ARM_STM32_SYSTICK_Delay_Ms(uint32_t val)
+{
+	//BLOCKING DELAY OF SPECIFIED MILLI-SECONDS
+	
+	ARM_STM32_SYSTICK_Delay_Us(val * 1000);
+}
+
--- ARM_STM32_SYSTICK.h
+++ ARM_STM32_SYSTICK.h
@@ -10,8 +10,18 @@
 #ifndef _ARM_STM32_SYSTICK_
 #define _ARM_STM32_SYSTICK_
 
-#define DEVICE_UNIQUE_ID_ADDRESS
+#include <stdint.h>
+#include "stm32f0xx_misc.h"
 
-void ARM_STM32_SYSTICK_Read_Device_Id(uint8_t* ptr);
+#define DEVICE_UNIQUE_ID_ADDRESS 0x1FFFF7AC
+#define STM_STM32_SYSTICK_SOURCE_HCLK SysTick_CLKSource_HCLK
+#define STM_STM32_SYSTICK_SOURCE_HCLK_DIV8 SysTick_CLKSource_HCLK_Div8
+
+static uint32_t ARM_STM32_SYSTICK_SOURCE;
+
+void ARM_STM32_SYSTICK_Read_Device_Id(uint32_t* ptr);
+void ARM_STM32_SYSTICK_Init(uint32_t systick_clock_source);
+void ARM_STM32_SYSTICK_Delay_Us(uint32_t val);
+void ARM_STM32_SYSTICK_Delay_Ms(uint32_t val);
 
 #endif
