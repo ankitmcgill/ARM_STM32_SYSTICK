5a5b3941d653c5ae43d0a6aa839aa36865c384d1
Ankit Bhatnagar <ankit.bhatnagarindia@gmail.com>
2017-02-26 16:11:26 +0530

    initial file committ

---
+++ ARM_STM32_SYSTICK.h
@@ -0,0 +1,17 @@
+/////////////////////////////////////////////////////
+// ARM - STM32 - SYSTICK LIBRARY
+//
+// FEBRUARY 26 2017
+//
+// ANKIT BHATNAGAR
+// ANKIT.BHATNAGARINDIA@GMAIL.COM
+/////////////////////////////////////////////////////
+
+#ifndef _ARM_STM32_SYSTICK_
+#define _ARM_STM32_SYSTICK_
+
+#define DEVICE_UNIQUE_ID_ADDRESS
+
+void ARM_STM32_SYSTICK_Read_Device_Id(uint8_t* ptr);
+
+#endif
